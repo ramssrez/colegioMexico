53601deb20424e33399d3cdb98e7144dd0641859
Raul Ramirez Perez <ramssrez@gmail.com>
2024-02-26 21:00:03 -0600

    Creación de los menus de los diferentes usuarios

---
+++ components/navigarionPF.php
@@ -0,0 +1,8 @@
+        <nav class="navbar">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Consultar</a></li>
+                <li><a href="#">Pagos</a></li>
+                <li><a href="#">Salir</a></li>
+            </ul>
+        </nav>
---
+++ components/navigationPDC.php
@@ -0,0 +1,8 @@
+        <nav class="navbar">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Consultar pagos</a></li>
+                <li><a href="#">Registrar pagos</a></li>
+                <li><a href="#">Salir</a></li>
+            </ul>
+        </nav>
