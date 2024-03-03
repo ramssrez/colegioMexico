cd0e32aaed6d3bc55695e389db980de32d6244fe
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-03 13:08:33 -0600

    Integración de la descripción del colegio

--- index.php
+++ index.php
@@ -12,5 +12,15 @@
         ?>    
         <h1 class="titulo">Colegio Instituto México</h1>      
         <img class="imagen" src="./img/escuela.png" alt="">  
+        <p class="descripcion-school">
+            El Colegio Instituto México, con domicilio en Av. De las Torres #780,
+            Alcaldía Cuauhtémoc, C.P. 06500, Ciudad de México; escuela de tradición con 50 años de experiencia
+            en la preparación de tus hijos con la mejor educación posible.
+        </p>
+        <p class="descripcion-school">
+            Contamos con diferentes planes de estudio, constantemente en cambio con las nuevas necesidades  
+            mundiales, enfocada en tener una mayor preparación de nuestros alumnos en los ámbitos técnicos, 
+            personales y financieros.
+        </p>
     </body>
 </html>
