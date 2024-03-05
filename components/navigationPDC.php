9ac9e2755878da73c9218f5df6640e67953acd87
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-04 18:43:12 -0600

    Integración de la dirección de las paginas de salida

--- components/navigationPDC.php
+++ components/navigationPDC.php
@@ -1,6 +1,6 @@
         <nav class="navbar">
             <ul>
-                <li><a href="#">Inicio</a></li>
+                <li><a href="../pages/homeUserPDC.php">Inicio</a></li>
                 <li><a href="#">Consultar pagos</a></li>
                 <li><a href="#">Registrar pagos</a></li>
                 <li><a href="#">Salir</a></li>
--- components/navigationPF.php
+++ components/navigationPF.php
@@ -1,6 +1,6 @@
         <nav class="navbar">
             <ul>
-                <li><a href="#">Inicio</a></li>
+                <li><a href="../pages/homeUserPF.php">Inicio</a></li>
                 <li><a href="#">Consultar pagos</a></li>
                 <li><a href="#">Salir</a></li>
             </ul>
