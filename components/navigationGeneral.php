ff123dd182e3d1baa1297690a877dc60f18f46e3
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-05 20:38:14 -0600

    Limpieza de la rama

--- components/navigationGeneral.php
+++
@@ -1,7 +0,0 @@
-        <nav class="navbar column">
-            <ul>
-                <li><a href="../../colegio-instituto-mexico/index.php">Inicio</a></li>
-                <li><a href="../../colegio-instituto-mexico/pages/registerUser.php">Registrarse</a></li>
-                <li><a href="../../colegio-instituto-mexico/pages/iniciarSession.php">Iniciar sesión</a></li>
-            </ul>
-        </nav>
--- components/navigationPDC.php
+++
@@ -1,8 +0,0 @@
-        <nav class="navbar">
-            <ul>
-                <li><a href="../pages/homeUserPDC.php">Inicio</a></li>
-                <li><a href="#">Consultar pagos</a></li>
-                <li><a href="#">Registrar pagos</a></li>
-                <li><a href="../config/logout.php">Salir</a></li>
-            </ul>
-        </nav>
