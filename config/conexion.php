f14202d1e5c9d9499e32d5148b23beb87927da78
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-05 19:50:46 -0600

    Integración de la sección de diseño del texto de presetación como db local

--- config/conexion.php
+++ config/conexion.php
@@ -1,9 +1,9 @@
 <?php
-    $pass_db = "";
+    $pass_db = "admin";
     $user_db = "root";
     $name_db = "db_instituto_mexico";
     try{
-        $conexion = new PDO('mysql:host=localhost:3308;dbname='.$name_db,$user_db,$pass_db);
+        $conexion = new PDO('mysql:host=localhost:3306;dbname='.$name_db,$user_db,$pass_db);
     }catch(Exception $ex){
         echo "Error de conexión a la base de datos ".$ex->getMessage();
     }
