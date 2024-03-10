ad39a22e60d4d16d99da666dde25569b4060f709
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-09 18:39:15 -0600

    Cambio de mensaje de bienvenida para los tipos de usuarios

--- pages/homeUserPF.php
+++ pages/homeUserPF.php
@@ -14,13 +14,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/styles.css">
-        <title>!Bienvenido <?php echo $client; ?></title>
+        <title>!Bienvenid@ <?php echo $client; ?></title>
     </head>
     <body>
         <?php
             include("../components/navigationPF.php");
         ?> 
-        <h1 class="bienvenido">!Bienvenido <?php echo $client; ?> <?php echo $_SESSION['apellidoPa']; ?> <?php echo $_SESSION['apellidoMa']; ?>!</h1>
+        <h1 class="bienvenido">!Bienvenid@ <?php echo $client; ?> <?php echo $_SESSION['apellidoPa']; ?> <?php echo $_SESSION['apellidoMa']; ?>!</h1>
         <h2 class="bienvenido-dos">¡Has ingresado como <?php echo $_SESSION['tipo']; ?>!</h2>
     </body>
 </html>
