1473a0be86e46ae2446d838d99f8c88c3da4e784
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-09 20:57:19 -0600

    Integración del busqueda del usuario, para que contenga pagos

--- pages/consultarPagosPDC.php
+++ pages/consultarPagosPDC.php
@@ -20,6 +20,28 @@
         <?php
             include("../components/navigationPDC.php");
         ?>
+        <div class="contenedor">
+            <h2>Búsqueda de pagos</h2>
+            <?php
+                if(isset($_GET['error']) && $_GET['error'] == 1 )
+                {
+                    echo "<p class='error'>*Error: El campo esta vacío.</p>";
+                } 
+                if(isset($_GET['error']) && $_GET['error'] == 2 )
+                {
+                    echo "<p class='error'>*Error: El usuario ingresado, no existe.</p>";
+                }  
+            ?>
+            <form action="../config/busquedaIDUsuario.php" method="POST">
+                <div class="elemento">
+                    <label for="usuario">IDUsuario</label>
+                    <input type="text" id="usuario" name="IDUser" >
+                </div>
+                <div class="elemento">
+                    <input type="submit" value="Búscar">
+                </div>
+            </form>
+        </div>
         <h1>Consulta de pagos</h1>
     </body>
 </html>
