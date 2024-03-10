128e1fe08fafee41a1d2627cd83abd760657d626
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-09 18:51:05 -0600

    Creación de la vista para la consulta de pagos de un usuario

--- pages/consultaPagosPF.php
+++ pages/consultaPagosPF.php
@@ -29,31 +29,40 @@
             $statement->execute();
             $dataPagos = $statement->fetchAll(PDO::FETCH_OBJ);
         ?>
-        <?php if ($dataPagos && count($dataPagos) > 0): ?>
-            <table>
-                <thead>
-                    <th>ID</th>
-                    <th>Folio</th>
-                    <th>Concepto</th>
-                    <th>Mes</th>
-                    <th>Monto</th>
-                    <th>Fecha</th>
-                </thead>
-                <tbody>
-                    <?php foreach ($dataPagos as $data): ?>
-                        <tr>
-                            <td><?php echo $data->id?></td>
-                            <td><?php echo $data->FolioPago; ?></td>
-                            <td><?php echo $data->Concepto; ?></td>
-                            <td><?php echo $data->MesPagado; ?></td>
-                            <td><?php echo $data->Monto; ?></td>
-                            <td><?php echo $data->FechaPago; ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php else: ?>
-            <p class="noData-user"><?php echo $client; ?>, aún no cuentas con registro de pagos</p>
-        <?php endif; ?>
+        <div class="table-crud">
+            <?php if ($dataPagos && count($dataPagos) > 0): ?>
+                <h2 class="bienvenido-dos">
+                    Padre de familia: <?php echo $_SESSION['idUser'];?>
+                </h2>
+                <h2 class="bienvenido-dos">
+                    Nombre: <?php echo $client; ?> <?php echo $_SESSION['apellidoPa']; ?> <?php echo $_SESSION['apellidoMa']; ?>.
+                </h2>
+                <h2 class="bienvenido-dos">Pagos realizados:</h2>
+                <table>
+                    <thead>
+                        <th>ID</th>
+                        <th>Folio</th>
+                        <th>Concepto</th>
+                        <th>Mes</th>
+                        <th>Monto</th>
+                        <th>Fecha</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($dataPagos as $data): ?>
+                            <tr>
+                                <td><?php echo $data->idPago; ?></td>
+                                <td><?php echo $data->FolioPago; ?></td>
+                                <td><?php echo $data->Concepto; ?></td>
+                                <td><?php echo $data->MesPagado; ?></td>
+                                <td><?php echo $data->Monto; ?></td>
+                                <td><?php echo $data->FechaPago; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php else: ?>
+                <p class="noData-user"><?php echo $client; ?>, aún no cuentas con registro de pagos</p>
+            <?php endif; ?>
+        </div>
     </body>
 </html>
