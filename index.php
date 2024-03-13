03be6b72d4f640c8456fb8b7c79306ead08fc4fe
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-12 20:26:19 -0600

    Integración de Bootstrap para el index

--- index.php
+++ index.php
@@ -3,24 +3,62 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>Colegio Instituto México</title>
     </head>
     <body>
-        <?php
-            include("./components/navigationGeneral.php");
-        ?>    
-        <h1 class="titulo">Colegio Instituto México</h1>      
-        <img class="imagen" src="./img/escuela.jpg" alt="">  
-        <p class="descripcion-school">
-            El Colegio Instituto México, con domicilio en Av. De las Torres #780,
-            Alcaldía Cuauhtémoc, C.P. 06500, Ciudad de México; escuela de tradición con 50 años de experiencia
-            en la preparación de tus hijos con la mejor educación posible.
-        </p>
-        <p class="descripcion-school">
-            Contamos con diferentes planes de estudio, constantemente en cambio con las nuevas necesidades  
-            mundiales, enfocada en tener una mayor preparación de nuestros alumnos en los ámbitos técnicos, 
-            personales y financieros.
-        </p>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Colegio Instituto México</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="../../colegio-instituto-mexico/index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../colegio-instituto-mexico/pages/registerUser.php">Registrarse</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../../colegio-instituto-mexico/pages/iniciarSession.php">Iniciar sesión</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> 
+        
+        <div class="container">
+            <div class="row">
+                <div class="col text-center m-2">
+                    <h1>Colegio Instituto México</h1> 
+                </div>
+            </div>
+        </div>   
+        <div class="container p-3">
+            <div class="row">
+                <div class="col text-center">
+                    <img src="./img/escuela.jpg" class="img-fluid" alt="Descripción de la imagen">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center m-2">
+                    <p class="text-justify">
+                        El Colegio Instituto México, con domicilio en Av. De las Torres #780,
+                        Alcaldía Cuauhtémoc, C.P. 06500, Ciudad de México; escuela de tradición con 50 años de experiencia
+                        en la preparación de tus hijos con la mejor educación posible.
+                    </p>
+                    <p class="text-justify">
+                        Contamos con diferentes planes de estudio, constantemente en cambio con las nuevas necesidades  
+                        mundiales, enfocada en tener una mayor preparación de nuestros alumnos en los ámbitos técnicos, 
+                        personales y financieros.
+                    </p>
+                </div>
+            </div>
+        </div>    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
