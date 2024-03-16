898752301583972927b2366214751b83039dce20
Raul Ramirez Perez <ramssrez@gmail.com>
2024-03-16 16:15:11 -0600

    Cambio de la ubicación de la funcionalidad de tootip

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-document.getElementById('usuario').addEventListener('click', function() {
+document.getElementById('idUserInput').addEventListener('click', function() {
     if(window.location.search.includes('successful=1')){
         var cleanURL = window.location.href.split('?')[0];
         window.location.href = cleanURL;
     }
 });
-document.getElementById('usuario').addEventListener('click', function() {
+document.getElementById('idUserInput').addEventListener('click', function() {
     if(window.location.search.includes('successful=2')){
         var cleanURL = window.location.href.split('?')[0];
         window.location.href = cleanURL;
     }
 });
-var tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
